45fbeb2d59d179b2b0c9ee733e1b48b29360db66
GalanCM <galan.c.m@gmail.com>
2016-08-28 16:25:08 -0400

    Remove debug prints.

--- Units/walk_grid.gd
+++ Units/walk_grid.gd
@@ -7,36 +7,35 @@ var attacks = []
 func _ready():
 	make_grid()
 
-	
+
 func make_grid():
 	for move in valid_moves:
 		var square = preload("res://Units/GridSquare.tscn").instance()
 		square.set_pos( get_node("/root/Level/TileMap").map_to_world( move ) )
 		add_child( square )
-		
-	print(attacks)
+
 	for attack in attacks:
 		var square = preload("res://Units/GridSquare-Enemy.tscn").instance()
 		square.set_pos( get_node("/root/Level/TileMap").map_to_world( attack ) )
 		add_child( square )
-		
+
 	var tween = Tween.new()
 	tween.interpolate_method( self, "set_opacity", 0, 1, 0.3, Tween.TRANS_QUAD, Tween.EASE_IN)
 	add_child(tween)
 	tween.start()
-	
+
 	yield( tween, 'tween_complete' )
 	tween.queue_free()
-		
-	
+
+
 func clear():
 	var tween = Tween.new()
 	tween.interpolate_method( self, "set_opacity", 1, 0, 0.3, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	add_child(tween)
 	tween.start()
-	
+
 	yield( tween, 'tween_complete' )
 	tween.queue_free()
-	
+
 	for child in get_children():
 		child.queue_free()
